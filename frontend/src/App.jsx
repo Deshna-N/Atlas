d5a55984deebec30dbe75b1248667b561317d5e6
deshnanaruka <d35hnanaruka@gmail.com>
2026-06-19 19:15:40 -0400

    Input for destination, start end dates, budget, notes created and stored on backend memory for now

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
 
 function App() {
-  const [destination, setDestination] = useState("");
-  const [message, setMessage] = useState("");
+  const [destination, setDestination] = useState("")
+  const [startDate, setStartDate] = useState("")
+  const [endDate, setEndDate] = useState("")
+  const [budget, setBudget] = useState("")
+  const [notes, setNotes] = useState("")
+  const [message, setMessage] = useState("")
 
   const createTrip = async () => {
     const response = await fetch("http://127.0.0.1:8000/trips", {
@@ -11,12 +15,12 @@ function App() {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        destination: destination,
-        start_date: "2026-12-15",
-        end_date: "2026-12-25",
-        budget: 2500,
-        notes: "Test trip",
-      }),
+      destination: destination,
+      start_date: startDate,
+      end_date: endDate,
+      budget: Number(budget),
+      notes: notes,
+    }),
     });
 
     const data = await response.json();
@@ -29,16 +33,51 @@ function App() {
       <h1>Atlas</h1>
 
       <input
-        placeholder="Destination"
-        value={destination}
-        onChange={(e) => setDestination(e.target.value)}
+      placeholder="Destination"
+      value={destination}
+      onChange={(e) => setDestination(e.target.value)}
+    />
+
+    <br /><br />
+
+      <input
+        type="date"
+        value={startDate}
+        onChange={(e) => setStartDate(e.target.value)}
       />
 
-      <button onClick={createTrip}>
-        Create Trip
-      </button>
+      <br /><br />
 
-      <p>{message}</p>
+      <input
+        type="date"
+        value={endDate}
+        onChange={(e) => setEndDate(e.target.value)}
+      />
+
+      <br /><br />
+
+      <input
+        type="number"
+        placeholder="Budget"
+        value={budget}
+        onChange={(e) => setBudget(e.target.value)}
+      />
+
+      <br /><br />
+
+      <textarea
+        placeholder="Notes"
+        value={notes}
+        onChange={(e) => setNotes(e.target.value)}
+      />
+
+      <br /><br />
+
+        <button onClick={createTrip}>
+          Create Trip
+        </button>
+
+        <p>{message}</p>
     </div>
   );
 }
