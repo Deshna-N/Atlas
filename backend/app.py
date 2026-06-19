0e078a5a4bd7082ab34f2f4e1bb1e7e671ca97b0
deshnanaruka <d35hnanaruka@gmail.com>
2026-06-19 18:54:54 -0400

    created base Atlas page with App.jsx

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Trip(BaseModel):
+    destination: str
+    start_date: str
+    end_date: str
+    budget: float
+    notes: str
 
 @app.get("/")
 def root():
     return {"message": "Welcome to Atlas"}
+
+@app.post("/trips")
+def create_trip(trip: Trip):
+    print(trip)
+
+    return {
+        "success": True,
+        "trip": trip
+    }
