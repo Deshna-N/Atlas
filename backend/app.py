49a9f87b3d82b3594ec7320c24b3c1e33b6e732d
deshnanaruka <d35hnanaruka@gmail.com>
2026-06-20 19:35:11 -0400

    Setup MongoDB Atlas integration

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+import os
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+load_dotenv()
+
+mongo_uri = os.getenv("MONGO_URI")
+
+client = MongoClient(mongo_uri)
+
+db = client["atlas"]
+
+trips_collection = db["trips"]
 
 app = FastAPI()
 
@@ -25,9 +38,12 @@ def root():
 
 @app.post("/trips")
 def create_trip(trip: Trip):
-    print(trip)
+
+    trip_dict = trip.model_dump()
+
+    result = trips_collection.insert_one(trip_dict)
 
     return {
         "success": True,
-        "trip": trip
+        "id": str(result.inserted_id)
     }
